c94934762dc3afd7a5daa77e438c23f0eb278ee6
Flash-0007 <flashgaming7804@gmail.com>
2025-01-17 20:33:26 +0530

    Still trying to add codes to repo

---
+++ outputinjava.java
@@ -0,0 +1,7 @@
+public class outputinjava {
+    public static void main(String[] args) {
+        System.out.print("Hello World!!!");
+    }
+}
+
+// ";" This is known as Statement Terminator
---
+++ sumofab.java
@@ -0,0 +1,7 @@
+public class sumofab {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+        System.out.print(a+b);
+    }
+}
